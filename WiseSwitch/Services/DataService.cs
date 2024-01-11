4d893377dc3db25b3421737d32f1ee156b59c562
Dario-97-d <dariodias97@gmail.com>
2024-01-11 11:13:56 +0000

    Minor tweaks (cont.)

--- WiseSwitch/Services/DataService.cs
+++ WiseSwitch/Services/DataService.cs
@@ -13,28 +13,28 @@
         // Get data.
         public async Task<T> GetAsync<T>(string url, object value)
         {
-            return await _apiService.GetDataFromApiAsync<T>(url + value);
+            return await _apiService.GetDataAsync<T>(url + value);
         }
 
 
         // Post data.
         public async Task CreateAsync(string url, object value)
         {
-            await _apiService.PostDataToApiAsync(url, value);
+            await _apiService.PostDataAsync(url, value);
         }
 
 
         // Put data.
         public async Task UpdateAsync(string url, object value)
         {
-            await _apiService.PutDataToApiAsync(url, value);
+            await _apiService.PutDataAsync(url, value);
         }
 
 
         // Delete data.
         public async Task<object> DeleteAsync(string url, object value)
         {
-            return await _apiService.DeleteDataFromApiAsync(url + value);
+            return await _apiService.DeleteDataAsync(url + value);
         }
     }
 }
